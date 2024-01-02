7ade4beb5853eed948e5615dee0df20e485e0054
Oguzhan <oguzhanbaysal@outlook.com>
2024-01-02 21:19:02 +0300

    zzzz

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -6,7 +6,7 @@ import { getPost } from "@/lib/data";
 
 // FETCH DATA WITH AN API
 const getData = async (slug) => {
-  const res = await fetch(`http://127.0.0.1:3000/api/blog/${slug}`);
+  const res = await fetch(`http://localhost:3000/api/blog/${slug}`);
 
   if (!res.ok) {
     throw new Error("Something went wrong");
